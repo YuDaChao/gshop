4805a9bbb9fbfabcdff03fb4b52bc55ba1868531
happyu <290945266@qq.com>
2018-06-27 22:37:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>App</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    color: #000
+</style>
