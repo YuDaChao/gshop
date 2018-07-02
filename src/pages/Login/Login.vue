90f2e2942e61d77b600f40984c6a2a84bba7d09d
happyu <290945266@qq.com>
2018-07-02 17:37:28 +0800

    完成登录切换显示

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,13 +4,14 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on: loginWay}" @click="loginWay = true">短信登录</a>
+          <a href="javascript:;" :class="{on: !loginWay}" @click="loginWay = false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <!--短信登录-->
+          <div :class="{on: loginWay}">
             <section class="login_message">
               <input type="tel" maxlength="11" placeholder="手机号">
               <button disabled="disabled" class="verification">获取验证码</button>
@@ -23,21 +24,22 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div style="display: none">
+          <!--密码登录-->
+          <div :class="{on: !loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
                 <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <div class="switch_button" :class="showPwd ? 'on' : 'off'" @click="showPwd = !showPwd">
+                  <div class="switch_circle" :class="{right: showPwd}"></div>
+                  <span class="switch_text">{{showPwd ? 'abc' : '***'}}</span>
                 </div>
               </section>
-              <section class="login_message">
+              <section class="login_message login_captcha">
                 <input type="text" maxlength="11" placeholder="验证码">
-                <img class="get_verification" src="./images/captcha.svg" alt="captcha">
+                <img class="verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
           </div>
@@ -54,7 +56,13 @@
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      loginWay: true, // true表示短信登录  false密码登录
+      showPwd: false // false不显示密码  true显示密码
+    }
+  }
 }
 </script>
 
@@ -79,14 +87,20 @@ export default {
           width 100%
           text-align center
           font-weight 700
+          & > a
+            margin-right 30px
           a
             font-size 14px
           .on
             color: #02a774
             border-bottom 2px solid #02a774
-            margin-right 30px
       .login_content
         margin-top 16px
+        >form
+          >div
+            display none
+            &.on
+              display block
         .login_message, .login_verification
           position relative
           height 48px
@@ -97,8 +111,10 @@ export default {
             height 100%
             border 1px solid #ddd
             border-radius 4px
-            padding-left: 4px
+            padding-left: 10px
             outline none
+            &:focus
+              border 1px solid #02a774
           .verification
             position absolute
             right 10px
@@ -108,8 +124,49 @@ export default {
             font-size 14px
             color: #ccc
             background-color transparent
-        .login_verification
+        .login_verification, .login_captcha
+          position relative
           margin-top 16px
+          .verification
+            right -10px
+          .switch_button
+            &.on
+              background-color #02a774
+              .switch_text
+                float left
+                color: #fff
+                padding-left 2px
+            &.off
+              background-color #fff
+              .switch_text
+                float right
+                color: #ccc
+                margin-top 2px
+          .switch_button
+            font-size 12px
+            border 1px solid #ddd
+            border-radius 8px
+            width 40px
+            height 16px
+            line-height 16px
+            color: #fff
+            position absolute
+            top 50%
+            right 10px
+            transform translateY(-50%)
+            .switch_circle
+              position absolute
+              top -1px
+              left -1px
+              width 16px
+              height 16px
+              border 1px solid #ddd
+              border-radius 50%
+              background-color #fff
+              box-shadow 0 2px 4px 0 rgba(0,0,0,0.1)
+              transition transform 0.3s
+              &.right
+                transform translateX(24px)
         .login_hint
           color: #999
           font-size 14px
